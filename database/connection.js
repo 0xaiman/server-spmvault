904e165c0e35e1bdcc74d235752661ec5e47921d
0xaiman <aimandani9797@gmail.com>
2024-06-21 04:34:03 +0800

    connection syntax @ connection.js, tally with .env

--- database/connection.js
+++ database/connection.js
@@ -13,14 +13,13 @@ const { Pool } = pkg;
 
 
 export const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database:process.env.PGDATABASE,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
+  
+  });
 
 async function dbInit(){
     try{
